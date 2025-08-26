b0cf9baa035376fed6ef034c1316a3d99d387cdd
AgustinPiergentili <agus.piergentili73@gmail.com>
2025-08-25 21:06:39 -0300

    se cambia contraseña

--- config/dbClient.js
+++ config/dbClient.js
@@ -3,7 +3,7 @@ import {MongoClient} from 'mongodb';
 
 class dbClient {
     constructor() {
-        const queryString = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.SERVER_DB}/?retryWrites=true&w=majority&appName=task-api`;
+        const queryString = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.SERVER_DB}?retryWrites=true&w=majority&appName=task-api`;
         this.client = new MongoClient(queryString);
         this.conectarDB();
     };
